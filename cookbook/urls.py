1650186d84705eda6ec5e5b7f95ece877de6801f
Cody Winchester <cjwinchester@gmail.com>
2018-07-29 16:19:39 -0600

    wrong path in urls

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('recipes/', include('recipes.urls')),
+    path('cookbook/', include('recipes.urls')),
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
 ]
